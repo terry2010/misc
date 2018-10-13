60570cc027b0faf89b1aeebe3dd83b7a3d5fe8ab
terry2010 <jstel@126.com>
2018-10-14 02:48:20 +0800

    Update function.php

--- function.php
+++ function.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * 老代码翻出来的一些函数,也不知道什么时候写的
+ */
 
 function array_slice_assoc($array, $offset, $length = null, $preserve_keys = FALSE) {
     $key = array_slice(array_keys($array), $offset, $length, $preserve_keys);
